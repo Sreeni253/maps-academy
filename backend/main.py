80dd834d4cb24d88c260f453fd54f2907b62fbed
Sreeni253 <deverapalli.sreenivasulu@outlook.com>
2026-02-09 14:47:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 from typing import Optional
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def get_academy_status() -> str:
